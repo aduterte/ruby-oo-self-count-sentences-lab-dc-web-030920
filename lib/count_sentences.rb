c4352508d0863be881b18c163856730ce7614ffa
aduterte <alexduterte@gmail.com>
2020-03-10 21:18:48 -0400

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,23 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    
+    # new_string = self.split (/\.|\!|\?/)
+   
+    # new_string.reject { |x| x.empty? }.count
+    self.split(/\.|\!|\?/).reject { |x| x.empty? }.count
+    
   end
 end
